4b3322710bb91f5669e54bdce81d1c051681ad0d
Juan-CamiloF <jucamilogac@outlook.com>
2020-10-08 17:42:07 -0500

    Se construyo el homo, nav y footer global

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/share/nav/nav.component';
+import { FooterComponent } from './components/share/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
